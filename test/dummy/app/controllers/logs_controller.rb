5ad73156c8fdaf986bae42277238ff9035992bbe
Alessandro Rodi <alessandro.rodi@renuo.ch>
2025-09-26 07:56:16 +0200

    Add logs#show

---
+++ test/dummy/app/controllers/logs_controller.rb
@@ -0,0 +1,30 @@
+class LogsController < ApplicationController
+  def show
+    encrypted_path = params[:file_path]
+
+    if encrypted_path.blank?
+      redirect_to root_path, alert: "No file specified"
+      return
+    end
+
+    begin
+      @log_file_path = Onlylogs::SecureFilePath.decrypt(encrypted_path)
+
+      unless File.exist?(@log_file_path)
+        redirect_to root_path, alert: "File not found"
+        return
+      end
+
+    rescue Onlylogs::SecureFilePath::SecurityError => e
+      Rails.logger.error "LogsController: Security violation - #{e.message}"
+      redirect_to root_path, alert: "Access denied"
+      return
+    end
+
+    @max_lines = (params[:max_lines] || 100).to_i
+    @filter = params[:filter]
+    @autoscroll = params[:autoscroll] != "false"
+    @mode = @filter.blank? ? (params[:mode] || "live") : "search"
+    @fast = params[:fast] == "true"
+  end
+end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount Onlylogs::Engine => "/onlylogs"
 
   root "home#show"
+  get "logs/:file_path", to: "logs#show", as: :log_viewer
 end
