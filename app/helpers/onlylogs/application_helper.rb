780dfe09b6ab778ff1cf27c34b98442ceec53467
Dani Bengl <daniel.bengl@renuo.ch>
2026-05-05 22:44:14 +0200

    Eliminate branch

--- app/helpers/onlylogs/application_helper.rb
+++ app/helpers/onlylogs/application_helper.rb
@@ -4,8 +4,6 @@ module Onlylogs
       pathname = Pathname.new(file)
       relative = pathname.relative_path_from(Rails.root).to_s
       relative.start_with?("..") ? pathname.basename.to_s : relative
-    rescue ArgumentError
-      Pathname.new(file).basename.to_s
     end
 
     def encrypted_log_file_path(file)
