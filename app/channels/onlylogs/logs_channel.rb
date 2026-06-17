14e58272ab49cd6e1484da712940b21ec60f148e
Jonathan Stalberg <jonathan.stalberg@renuo.ch>
2026-06-17 10:01:39 +0200

    Dont use grep when getting the range

--- app/channels/onlylogs/logs_channel.rb
+++ app/channels/onlylogs/logs_channel.rb
@@ -171,10 +171,6 @@ module Onlylogs
         last_byte_offset = nil
         line_count = 0
 
-        # Adjust start position to beginning of next complete line
-        adjusted_start = (start_position > 0) ? next_line_boundary(file_path, start_position) : 0
-        adjusted_end = end_position ? next_line_boundary(file_path, end_position) : nil
-
         Rails.logger.silence(Logger::ERROR) do
           if filter.present?
             # Use grep for filtered search
